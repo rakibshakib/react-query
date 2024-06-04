abd9149df1802583724274b23c198f7544776936
iboscopilot <eaccount@ibos.io>
2024-06-04 15:36:49 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-purple-500">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
